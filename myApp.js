f438895796c579997c14c9f2d5a0c1cdbc1defab
Matt <mattmleach@gmail.com>
2019-06-17 15:56:53 -0400

    updated

--- myApp.js
+++ myApp.js
@@ -12,6 +12,8 @@
 // `mongoose`. Store your **mLab** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
 
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
